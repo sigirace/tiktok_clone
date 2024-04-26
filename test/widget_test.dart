e0e722b2832eaa4404b2cd5f204f64cd5a186380
sigirace <chickang9@gmail.com>
2024-04-26 05:12:52 +0000

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tictok_clone/main.dart';
 
+//Test
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
